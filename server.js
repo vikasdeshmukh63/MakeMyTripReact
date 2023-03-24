3e8569ec9e97eec610e241c92f7b21fa8861afaa
Vikas Deshmukh <114295008+vikasdeshmukh63@users.noreply.github.com>
2023-03-24 14:59:20 +0530

    deploy req fullfil

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use("/api/users", usersRoute);
 app.use("/api/buses",busesRoute);
 app.use("/api/bookings",bookingsRoute);
 
+const path = require("path");
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client/build/index.html"));
+    });
+}
+
 app.listen(port, () => {
     console.log(`server is started on port ${port}`);
 });
