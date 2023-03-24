f2d640d42b261d6fceb9af177f0ab1f5e98e7cdf
Vikas Deshmukh <114295008+vikasdeshmukh63@users.noreply.github.com>
2023-03-24 15:13:53 +0530

    heroku removed

--- server.js
+++ server.js
@@ -13,13 +13,7 @@ app.use("/api/users", usersRoute);
 app.use("/api/buses",busesRoute);
 app.use("/api/bookings",bookingsRoute);
 
-const path = require("path");
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"client/build/index.html"));
-    });
-}
+
 
 app.listen(port, () => {
     console.log(`server is started on port ${port}`);
